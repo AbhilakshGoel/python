f116b66e167d1fb1ad12959842c22e813c8f2606
Abhi <abhilakshgoel@gmail.com>
2021-01-03 21:20:36 +0530

    Renamed Some Files

--- Excercise 4.py
+++
@@ -1,9 +0,0 @@
-"""Astrologer Stars"""
-n = int(input("How Many Rows of Stars do you want: "))
-bool = bool(int(input("Enter a number 1 or 0: ")))
-if bool==True:
-    for i in range(0, n+1):
-        print(i*"* ", end=" \n")
-elif bool==False:
-    for i in range(n, 0,-1):
-        print(i * "* ", end=" \n")
